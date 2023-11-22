e24d06b674c55591ca6fdae048edb23727eb163b
SuperSection <soumosarkar297@gmail.com>
2023-11-22 20:54:08 +0530

    setup Sidebar navigation at Main Documents page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,8 @@ import { redirect } from "next/navigation";
 
 import { Spinner } from "@/components/spinner";
 
+import { Navigation } from "./_components/navigation";
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
   if (isLoading) {
@@ -21,6 +23,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
       <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
   );
